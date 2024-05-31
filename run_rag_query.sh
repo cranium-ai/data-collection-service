dfdbea635da83352dc7eefed2cc030f331bc0ec1
rroycranium <156364868+rroycranium@users.noreply.github.com>
2024-05-31 16:03:05 -0400

    Delete run_rag_query.sh

--- run_rag_query.sh
+++
@@ -1,6 +0,0 @@
-# python threat_rag_query.py "What are the ways that adversaries can attack your system ?"
-# python threat_rag_query.py "What are the AI System level Vulnerabilities with open-source models ?"
-
-python threat_rag_query.py "provide unique list of data_source"
-#python threat_rag_query.py "What are the AI System level Vulnerabilities with open-source models ?"
-
